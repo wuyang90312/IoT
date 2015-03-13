de0b59049b384bfd100673cde1a95c45bfe00947
Yang <wuyangcanada@hotmail.com>
2015-03-13 15:45:02 -0400

    Change the serial communication from software to hardware => change from myserial to the default Serial1

--- Arduino+ESP/IoTCommu/IoTCommu.ino
+++ Arduino+ESP/IoTCommu/IoTCommu.ino
@@ -1,38 +1,31 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial mySerial(11, 12); //RX,TX
+//SoftwareSerial mySerial(11, 12); //RX,TX
 
 void setup()
 {
-  Serial.begin(9600);    // Arduino serial Monitor
+  Serial.begin(115200);    // Arduino serial Monitor
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
 
-  mySerial.begin(9600);   // The ESP8826 has a baud rate of 115200
+  Serial1.begin(57600);   // The ESP8826 has a baud rate of 115200
 
-    // Enable esp8266 
+  // Enable esp8266 
   pinMode(13, OUTPUT);
   digitalWrite(13, HIGH);
 
-  CommLaunch("AT\r", 1000, true);
+ // CommLaunch("AT\r", 1000, true);
   CommLaunch("AT+RST\r", 4*1000, true);
-  CommLaunch("AT+CIOBAUD?\r", 4*1000, true);
+ // CommLaunch("AT+CIOBAUD=57600\r", 4*1000, true);
+  delay(5*1000);
+ // Serial1.begin(57600);   // The ESP8826 has a baud rate of 115200
+ // CommLaunch("AT+RST\r", 4*1000, true);
   
   CommLaunch("AT+CWMODE=3\r", 1000, true);
   CommLaunch("AT+CIFSR\r", 4*1000, true);
   CommLaunch("AT+CWLAP\r", 4*1000, true);
-  
-  
- /* String tmp;
-  
-  tmp="The Seeed tutorial is writen for the version 1 module with a 57600 baud\n";
-  tmp+= "rate serial link. The version 2 module that is currently shipping runs\n";
-  tmp+="at 115200 bps Also, the version 2 module requires 5 connections rather\n";
-  tmp+="than the 4 shown in the tutorial. You need to pull the chip-enable (CH_PD)\n";
-  tmp+="pin high - by connecting it to a 3.3V source via a (say) 10k resistor.\n";
-  
-  Serial.print(tmp);*/
+
 }
 
 void loop() {
@@ -42,12 +35,17 @@ void loop() {
 
 boolean CommLaunch(String cmd,unsigned int duration, boolean res)
 {
-  mySerial.println(cmd);    //send command to ESP8266
+  Serial1.println(cmd);    //send command to ESP8266
   //mySerial.flush();  //wait for the command finshes the sending
   /* Display the Command on the Moniter */
     Serial.println(cmd);
   if(res)
     CommResponse("sth", duration);
+}
+
+void setUp()
+{
+  
 }
 
 void CommResponse(String keyword, unsigned int duration)
@@ -56,10 +54,10 @@ void CommResponse(String keyword, unsigned int duration)
   long deadline = millis()+duration;
   while(millis() < deadline)
   {
-    while (mySerial.available()>0)  {
+    while (Serial1.available()>0)  {
       //tmp += char(mySerial.read());
       //delay(15);
-      Serial.print(char(mySerial.read()));
+      Serial.print(char(Serial1.read()));
     }
 
     //if(tmp.length() > 0) {
